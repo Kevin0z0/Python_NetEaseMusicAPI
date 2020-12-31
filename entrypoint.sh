8186117e034abf36b61192e95729a73f8ec9d524
Restyled.io <commits@restyled.io>
2020-12-31 08:32:35 +0000

    Restyled by shellharden

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 MODULE_NAME=${MODULE_NAME:-netease.wsgi}
 VARIABLE_NAME=${VARIABLE_NAME:-application}
-export APP_MODULE=${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}
+export APP_MODULE="${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}"
 
 if [ -f /app/gunicorn_conf.py ]; then
     DEFAULT_GUNICORN_CONF=/app/gunicorn_conf.py
@@ -12,6 +12,6 @@ elif [ -f /app/app/gunicorn_conf.py ]; then
 else
     DEFAULT_GUNICORN_CONF=/gunicorn_conf.py
 fi
-export GUNICORN_CONF=${GUNICORN_CONF:-$DEFAULT_GUNICORN_CONF}
+export GUNICORN_CONF="${GUNICORN_CONF:-$DEFAULT_GUNICORN_CONF}"
 
 exec "$@"
--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ set -e
 # If there's a prestart.sh script in the /app directory, run it before starting
 PRE_START_PATH=/app/prestart.sh
 echo "Checking for script in $PRE_START_PATH"
-if [ -f $PRE_START_PATH ] ; then
+if [ -f "$PRE_START_PATH" ] ; then
     echo "Running script $PRE_START_PATH"
     . "$PRE_START_PATH"
 else 
