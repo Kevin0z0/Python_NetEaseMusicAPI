8b834f30f325d15b4d8b7f7f783070c1df60ef27
Restyled.io <commits@restyled.io>
2020-12-31 08:32:37 +0000

    Restyled by shfmt

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,11 @@ VARIABLE_NAME=${VARIABLE_NAME:-application}
 export APP_MODULE="${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}"
 
 if [ -f /app/gunicorn_conf.py ]; then
-    DEFAULT_GUNICORN_CONF=/app/gunicorn_conf.py
+  DEFAULT_GUNICORN_CONF=/app/gunicorn_conf.py
 elif [ -f /app/app/gunicorn_conf.py ]; then
-    DEFAULT_GUNICORN_CONF=/app/app/gunicorn_conf.py
+  DEFAULT_GUNICORN_CONF=/app/app/gunicorn_conf.py
 else
-    DEFAULT_GUNICORN_CONF=/gunicorn_conf.py
+  DEFAULT_GUNICORN_CONF=/gunicorn_conf.py
 fi
 export GUNICORN_CONF="${GUNICORN_CONF:-$DEFAULT_GUNICORN_CONF}"
 
